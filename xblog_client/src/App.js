bfac869d415b8ebae081dc0838481095100f8dc6
OsimFavour <famos204@gmail.com>
2023-12-26 00:35:58 +0100

    Add Home Component

--- xblog_client/src/App.js
+++ xblog_client/src/App.js
@@ -2,6 +2,7 @@ import { Container, CssBaseline } from '@mui/material';
 import { BrowserRouter, Routes } from 'react-router-dom'
 import Header from './Components/Header'
 import Footer from './Components/Footer';
+import Home from './Components/Home';
 
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
       <CssBaseline />
       <Header/>
       
-      
+      <Home/>
       <Footer/>
     </Container>
     
